ff57c6bd2ddfbce26c7760ccd5580b614b6f9f97
Mackewinsson <Mackewinsson@gmail.com>
2020-07-16 18:32:20 -0400

    landing page in place

---
+++ routes/cursodemicroblading.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.render("cursodemicroblading", {
+    title: "Curso de microblading",
+    cursos: [
+      {
+        nombre: "Online",
+        fecha: "Acceso inmediato",
+        lugar: "En linea",
+        horario: "Tu elijes",
+      },
+      {
+        nombre: "Online PRO",
+        fecha: "Acceso inmediato",
+        lugar: "En linea",
+        horario: "Tu elijes",
+      },
+      {
+        nombre: "Grupal GOLD",
+        fecha: "Por reserva",
+        lugar: "Sede Bellas Cejas",
+        horario: "10am a 6pm",
+      },
+      {
+        nombre: "Personalizado GOLD",
+        fecha: "Por reserva",
+        lugar: "Sede Bellas Cejas",
+        horario: "10am a 6pm",
+      },
+    ],
+  });
+});
+
+module.exports = router;
