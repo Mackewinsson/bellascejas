20ada5a16a5d4dd0605762aa8a90d8fbdf2cedb2
Mackewinsson <Mackewinsson@gmail.com>
2020-08-06 20:24:31 -0400

    prices and caractherictics rendered dinamicaly

--- routes/cursodemicroblading.js
+++ routes/cursodemicroblading.js
@@ -3,32 +3,62 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.render("cursodemicroblading", {
+  res.status(200).render("cursodemicroblading", {
     title: "Curso de microblading",
     cursos: [
       {
         nombre: "Online",
+        precio: "150.000",
         fecha: "Acceso inmediato",
         lugar: "En linea",
         horario: "Tu elijes",
+        caracteristicas: [
+          "12 hrs de video",
+          "10 Ejercicios practicos",
+          "Preguntas via correo electronico",
+          "Diploma de certificacion",
+        ],
       },
       {
         nombre: "Online PRO",
+        precio: "250.000",
         fecha: "Acceso inmediato",
         lugar: "En linea",
         horario: "Tu elijes",
+        caracteristicas: [
+          "12 hrs de video",
+          "10 Ejercicios practicos",
+          "Asesoria directa via Whatsapp y llamadas",
+          "Diploma de certificacion",
+        ],
       },
       {
         nombre: "Grupal GOLD",
+        precio: "500.000",
         fecha: "Por reserva",
         lugar: "Sede Bellas Cejas",
         horario: "10am a 6pm",
+        caracteristicas: [
+          "Curso online incluido",
+          "Dos dias de practicas presenciales con la profesora",
+          "Asesoria directa via whatsapp y llamadas",
+          "Minimo de 5 alumnas",
+          "Diploma de certificacion",
+        ],
       },
       {
         nombre: "Personalizado GOLD",
+        precio: "800.000",
         fecha: "Por reserva",
         lugar: "Sede Bellas Cejas",
         horario: "10am a 6pm",
+        caracteristicas: [
+          "Curso online incluido",
+          "Dos dias de practicas presenciales con la profesora",
+          "Asesoria directa via whatsapp y llamadas",
+          "Maximo 2 alumnas",
+          "Diploma de certificacion",
+        ],
       },
     ],
   });
