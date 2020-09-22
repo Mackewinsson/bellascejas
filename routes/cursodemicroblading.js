a7eb0f819cebf33c05fd35140415aa2b08d6a317
Mackewinsson <mackewinsson@gmail.com>
2020-09-21 21:09:49 -0300

    create new route

--- routes/cursodemicroblading.js
+++ routes/cursodemicroblading.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+// Arreglar botones
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.status(200).render("cursodemicroblading", {
